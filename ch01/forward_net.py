b4f6add68c795cd38495928f465fbd8a952ebc5d
hirokishingu <hrk.bass.526@gmail.com>
2018-07-21 07:57:33 +0900

    add forward_net and common

--- ch01/forward_net.py
+++ ch01/forward_net.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import numpy as np
 
 class Sigmoid:
@@ -15,3 +16,32 @@ class Affine:
         W, b = self.params
         out = np.dot(x, W) + b
         return out
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+x = np.random.randn(10, 2)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+print(s)

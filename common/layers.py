7e596dc4c0c3f77ca5d4be560f44f050ca894f28
hirokishingu <hrk.bass.526@gmail.com>
2018-07-21 10:00:01 +0900

    add two layers

--- common/layers.py
+++ common/layers.py
@@ -55,3 +55,29 @@ class Affine:
         self.grads[1][...] = db
         return dx
         
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx

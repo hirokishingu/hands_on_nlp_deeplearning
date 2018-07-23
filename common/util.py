d460f9c5579758b67046a2fb6781da6b2a7a20cd
hirokishingu <hrk.bass.526@gmail.com>
2018-07-23 22:03:31 +0900

    add cos_similarity

--- common/util.py
+++ common/util.py
@@ -40,5 +40,8 @@ def create_to_matrix(corpus, vocab_size, window_size=1):
     
     return co_matrix
 
-
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
 

beacfedab11db8173f16effb04e7e6559819607e
hirokishingu <hrk.bass.526@gmail.com>
2018-07-23 22:01:17 +0900

    add c matrix in common/util

--- common/util.py
+++ common/util.py
@@ -20,3 +20,25 @@ def preprocess(text):
     corpus = np.array([word_to_id[w] for w in words])
 
     return corpus, word_to_id, id_to_word
+
+def create_to_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+            
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+    
+    return co_matrix
+
+
+

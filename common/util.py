62533d39f80abf952621f6ea03addfe8baffd8fe
hirokishingu <hrk.bass.526@gmail.com>
2018-07-24 23:07:30 +0900

    add most similar

--- common/util.py
+++ common/util.py
@@ -45,3 +45,29 @@ def cos_similarity(x, y, eps=1e-8):
     ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
     return np.dot(nx, ny)
 
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print("%s is not found" % query)
+        return
+
+    print("\n[query] " + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        
+        print(" %s %s " % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
+

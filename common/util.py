6f0b00d4a0d0c1a3f8ebb3ba995a3260d772b9d7
hirokishingu <hrk.bass.526@gmail.com>
2018-07-25 22:09:46 +0900

    add

--- common/util.py
+++ common/util.py
@@ -21,7 +21,7 @@ def preprocess(text):
 
     return corpus, word_to_id, id_to_word
 
-def create_to_matrix(corpus, vocab_size, window_size=1):
+def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 

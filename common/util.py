225eae3546b4f1e4de68b047c0a8ba58c820cad3
hirokishingu <hrk.bass.526@gmail.com>
2018-08-04 08:24:01 +0900

    add create_contexts_target

--- common/util.py
+++ common/util.py
@@ -88,3 +88,17 @@ def ppmi(C, verbose=False, eps=1e-8):
                 if cnt % (total//100) == 0:
                     print('%.1f%% done' % (100*cnt/total))
     return M
+
+def create_contexts_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(-window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)

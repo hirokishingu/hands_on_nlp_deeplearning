bbaf4735a8d6d1669de69b9f64c02bef755cf775
hirokishingu <hrk.bass.526@gmail.com>
2018-08-11 07:32:28 +0900

    add

--- ch03/train.py
+++ ch03/train.py
@@ -14,3 +14,4 @@ text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
 
 vocab_size = len(word_to_id)
+contexts, target = create_contexts_target(corpus, window_size)

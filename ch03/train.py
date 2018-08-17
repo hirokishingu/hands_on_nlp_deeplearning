d22524ff9032e60f27ab4d3833dd8a6234308298
hirokishingu <hrk.bass.526@gmail.com>
2018-08-17 21:48:27 +0900

    add

--- ch03/train.py
+++ ch03/train.py
@@ -24,3 +24,6 @@ trainer = Trainer(model, optimizer)
 
 trainer.fit(contexts, target, max_epoch, batch_size)
 trainer.plot()
+word_vecs = model.word_vecs
+for word_id, word in id_to_word.items():
+    print(word, word_vecs[word_id])

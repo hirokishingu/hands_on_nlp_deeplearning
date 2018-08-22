fbe535bd3de93220b396229504ca49906b481f39
hirokishingu <hrk.bass.526@gmail.com>
2018-08-22 23:01:11 +0900

    add

--- common/layers.py
+++ common/layers.py
@@ -90,8 +90,14 @@ class Embedding:
         self.grads = [np.zeros_like(W)]
         self.idx = None
 
-    def forward(self, idx)
+    def forward(self, idx):
         W, = self,params
         self.idx = idx
         out = W[idx]
         return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        dW[self.idx] = dout
+        return None
